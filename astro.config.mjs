1fb111e3e5d6a6ee8666057c0402d5a416e2f9f4
Brad Davis <bcd296@nyu.edu>
2025-08-23 14:03:44 -0400

    github pages stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://bcdrom.com'
+  //site: 'http://bcdrom.com'
+  site: 'https://brad-davis.github.io',
+  base: '/creative-portfolio-astro'
 });
