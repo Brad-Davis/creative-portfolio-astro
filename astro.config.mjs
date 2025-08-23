09f8eed409e81cf8d7de68e87f0fab871ff47f43
Brad Davis <bcd296@nyu.edu>
2025-08-23 14:05:31 -0400

    custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  //site: 'http://bcdrom.com'
-  site: 'https://brad-davis.github.io',
-  base: '/creative-portfolio-astro'
+  site: 'http://bcdrom.com'
 });
